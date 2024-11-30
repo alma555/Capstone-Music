7a3aea17b87cc3a34bed215a8f07dde38cf11ed1
Alma <almathompson@protonmail.com>
2024-11-30 11:09:51 -0500

    finished a script for fetching list of related artists through the musicbrainz API

--- MusicBrainzscript.py
+++ MusicBrainzscript.py
@@ -14,24 +14,34 @@ def get_member_projects(artist_id):
     get_relations(artist_id)
     return 0
 
-#overall system
-#get other projects of band members
-#get albums by * (3)
-#get album by bands they played with 
-#get albums by (2)
+def get_results(artist_id):
+    project_members = get_relations(artist_id)
+    results = []
+    # it would be wise to rewrite this as a recursive function
+    for i in project_members:
+        depth_one = get_relations(i['artist']['id']) #bands members are in
+        for j in depth_one:
+            results.append(j)
+            j['artist']['id']
+            depth_two = get_relations(j['artist']['id']) #members of bands members are in
+            for k in depth_two:
+                results.append(k)
+                depth_three = get_relations(k['artist']['id']) #bands of members of bands members are in
+                for l in depth_three:
+                    results.append(l)
+    return results
+
+def return_artist_list(db_list):
+   names_list = []
+   for i in db_list:
+       names_list.append(i['artist']['name'])
+   names_list = list(set(names_list)) #remove duplicates
+   return names_list
 
 # testing prompt; will eventually be user input
 prompt = "Mountain Goats"
 mb.set_useragent("capstone testing", 0.1, "almathompson@protonmail.com")
 
-known = []
 search = mb.search_artists(artist=prompt)
 artist_id = search['artist-list'][0]['id']
-members = get_relations(artist_id)
-for i in members:
-    depth_one = get_relations(i['artist']['id'])
-    known.append(i['artist']['id'])
-    for j in depth_one:
-        if j['artist']['id'] in known:
-            print('found')
-        depth_two = get_relations(j['artist']['id'])
+print(return_artist_list(get_results(artist_id)))
