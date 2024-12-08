47e6e452fc2b8bf17e9a10078cd7552b8dc847fb
Alma <almathompson@protonmail.com>
2024-12-08 10:16:05 -0500

    musicbrainz fetching script

--- MusicBrainzscript.py
+++ MusicBrainzscript.py
@@ -10,38 +10,43 @@ def get_relations(artist_id):
 def get_release_groups(artist_id):
     return mb.get_artist_by_id(artist_id, includes=['release-groups'], release_type=['album', 'ep'])['artist']['release-group-list']
 
-def get_member_projects(artist_id):
-    get_relations(artist_id)
-    return 0
-
 def get_results(artist_id):
     project_members = get_relations(artist_id)
     results = []
     # it would be wise to rewrite this as a recursive function
-    for i in project_members:
-        depth_one = get_relations(i['artist']['id']) #bands members are in
-        for j in depth_one:
-            results.append(j)
-            j['artist']['id']
-            depth_two = get_relations(j['artist']['id']) #members of bands members are in
-            for k in depth_two:
-                results.append(k)
-                depth_three = get_relations(k['artist']['id']) #bands of members of bands members are in
-                for l in depth_three:
-                    results.append(l)
+    depth_one = [random.choice(project_members),random.choice(project_members),random.choice(project_members),
+                random.choice(project_members),random.choice(project_members),random.choice(project_members),
+                random.choice(project_members),random.choice(project_members),random.choice(project_members)]
+    for i in depth_one:
+        depth_two = get_relations(i['artist']['id']) #bands members are in
+        depth_two = random.choice(depth_two)
+        depth_three = random.choice(get_relations(depth_two['artist']['id'])) #members of bands members are in
+        depth_four = random.choice(get_relations(depth_three['artist']['id'])) #bands of members of bands members are in
+        results.append(depth_four)
     return results
 
 def return_artist_list(db_list):
    names_list = []
    for i in db_list:
-       names_list.append(i['artist']['name'])
-   names_list = list(set(names_list)) #remove duplicates
+       names_list.append(i)
    return names_list
 
-# testing prompt; will eventually be user input
-prompt = "Mountain Goats"
-mb.set_useragent("capstone testing", 0.1, "almathompson@protonmail.com")
+def produce_list(prompt):
+    final_list = []
+    search = mb.search_artists(artist=prompt)
+    artist_id = search['artist-list'][0]['id']
+    artist_list = return_artist_list(get_results(artist_id))
+    for i in artist_list:
+        if i['artist']['id'] != artist_id:
+            releases = get_release_groups(i['artist']['id'])
+            if releases != []:
+                pick = random.choice(releases)
+                final_list.append(pick['title'])
+    return final_list
 
-search = mb.search_artists(artist=prompt)
-artist_id = search['artist-list'][0]['id']
-print(return_artist_list(get_results(artist_id)))
+mb.set_useragent("capstone testing", 0.1, "almathompson@protonmail.com")
+# testing prompt; will eventually be user input
+prompt = "The Killers"
+
+final_list = produce_list(prompt)
+print(final_list)
--- app.py
+++ app.py
@@ -6,13 +6,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 # Initialize Flask app
 app = Flask(__name__)
 
-# Spotify API credentials (replace with your own)
-SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
-SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 
-# Initialize Spotipy client
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID,
-                                                           client_secret=SPOTIPY_CLIENT_SECRET))
 
 # Route to search for related music data
 @app.route('/search', methods=['GET'])
